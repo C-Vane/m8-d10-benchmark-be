9c4c3d1303864a10f7d01362c59b1cf6e204f6a4
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2021-02-26 12:54:07 +0100

    set up backend

---
+++ src/services/auth/oauth.js
@@ -0,0 +1,45 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const UserModel = require("../users/schema");
+const { authenticate } = require("./tools");
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (request, accessToken, refreshToken, profile, next) => {
+      const newUser = {
+        googleId: profile.id,
+        name: profile.name.givenName,
+        surname: profile.name.familyName,
+        email: profile.emails[0].value,
+        img: "https://thumbs.dreamstime.com/b/default-avatar-profile-trendy-style-social-media-user-icon-187599373.jpg",
+      };
+
+      try {
+        const user = await UserModel.findOne({ googleId: profile.id });
+
+        if (user) {
+          const tokens = await authenticate(user);
+          next(null, { user, tokens });
+        } else {
+          const createdUser = new UserModel(newUser);
+          const savedUser = await createdUser.save();
+          const tokens = await authenticate(savedUser);
+          next(null, { user: savedUser, tokens });
+        }
+      } catch (error) {
+        console.log(error);
+        next(error);
+      }
+    }
+  )
+);
+
+passport.serializeUser(function (user, next) {
+  next(null, user);
+});
